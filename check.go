e977f196b04e3a3067167ee53ed8e795b84b50d7
Cody Zacharias <22013186+haccer@users.noreply.github.com>
2018-06-05 01:07:04 -0400

    Create check.go

---
+++ check.go
@@ -0,0 +1,98 @@
+package available
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/domainr/whois"
+	"golang.org/x/net/publicsuffix"
+)
+
+func Domain(domain string) (available bool, badtld bool) {
+	available = false
+
+	domain = strings.ToLower(domain)
+
+	tld, icann := publicsuffix.PublicSuffix(domain)
+	badtld = badTLD(tld)
+
+	if icann == true {
+		query, err := publicsuffix.EffectiveTLDPlusOne(domain)
+		if err != nil {
+			return
+		}
+
+		if !badtld {
+			available = match(tld, getWhois(query))
+		}
+	}
+
+	return available, badtld
+}
+
+func match(tld, resp string) (available bool) {
+	available = false
+
+	fp := fingerprints()
+
+	// .ca & .lt have opposite fingerprints
+	if tld == "ca" || tld == "lt" {
+		if !strings.Contains(resp, fp[tld]) {
+			available = true
+		}
+	}
+
+	/* Checks if the .tld is in our fingerprint list
+	Then checks if the fingerprint is in the whois
+	response data */
+	if fp[tld] != "" {
+		if strings.Contains(resp, fp[tld]) {
+			available = true
+		}
+	} else {
+		/* If the .tld isn't in our fingerprint list,
+		this is the last resort options to check a
+		list of possible responses.*/
+		afp := all_fingerprints()
+
+		for _, f := range afp {
+			if strings.Contains(resp, f) {
+				available = true
+				break
+			}
+		}
+	}
+
+	return available
+}
+
+// Makes whois request, returns resposne as string
+func getWhois(domain string) (response string) {
+	req, err := whois.NewRequest(domain)
+	if err != nil {
+		return
+	}
+
+	resp, err := whois.DefaultClient.Fetch(req)
+	if err != nil {
+		return
+	}
+
+	return fmt.Sprintf("%s", resp)
+}
+
+// Checks if the TLD is apart of our "bad tld" list
+func badTLD(tld string) (bad bool) {
+	bad = false
+
+	tlds := badtlds()
+
+	for _, badtld := range tlds {
+		if tld == badtld {
+			bad = true
+			break
+		}
+	}
+
+	return bad
+}
